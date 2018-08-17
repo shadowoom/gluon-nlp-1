70d8193f321b8cf63ea31949b7c7973528854811
Chenguang Wang <chgwang@amazon.com>
2018-08-17 16:54:37 -0700

    add temporal api

--- scripts/nmt/__init__.py
+++ scripts/nmt/__init__.py
@@ -19,3 +19,17 @@
 
 # pylint: disable=wildcard-import
 """NMT example."""
+
+from . import bleu, dataset, encoder_decoder, gnmt, loss, transformer, translation, utils
+
+from .bleu import *
+from .dataset import *
+from .encoder_decoder import *
+from .gnmt import *
+from .loss import *
+from .transformer import *
+from .translation import *
+from .utils import *
+
+__all__ = bleu.__all__ + dataset.__all__ + encoder_decoder.__all__ + gnmt.__all__ + loss.__all__
++ transformer.__all__ + translation.__all__ + utils.__all__
